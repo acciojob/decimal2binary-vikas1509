b2688890ee917c2e6b9dd2aa5ba224a95f829ec1
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-08-01 00:22:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function decimalToBinary(num) {
-  //Write you code here
-  
+
+
+function decimalToBinary(decimalNumber) {
+  if (decimalNumber === 0) {
+    return "0"; // Special case for input 0
+  }
+
+  let binary = "";
+  while (decimalNumber > 0) {
+    binary = (decimalNumber % 2) + binary;
+    decimalNumber = Math.floor(decimalNumber / 2);
+  }
+
+  return binary;
 }
+
+// Example usage:
+const decimalNumber = 25;
+const binaryNumber = decimalToBinary(decimalNumber);
+console.log("Binary representation of", decimalNumber, "is", binaryNumber);
 
 window.decimalToBinary = decimalToBinary;
